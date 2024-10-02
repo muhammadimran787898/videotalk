3ce878bf18ca88a61d6513ac36f01cd71d7a7f8d
Muhammad imran <muhammadimran787898@gmail.com>
2024-10-03 01:40:00 +0530

    Deploy-Render

--- context/socket.js
+++ context/socket.js
@@ -4,26 +4,36 @@ import { io } from "socket.io-client";
 const SocketContext = createContext(null);
 
 export const useSocket = () => {
-    const socket = useContext(SocketContext)
-    return socket
+    const socket = useContext(SocketContext);
+    return socket;
 }
 
 export const SocketProvider = (props) => {
-  const { children } = props;
-  const [socket, setSocket] = useState(null);
+    const { children } = props;
+    const [socket, setSocket] = useState(null);
 
-  useEffect(() => {
-    const connection = io();
-    console.log("socket connection", connection)
-    setSocket(connection);
-  }, []);
+    useEffect(() => {
+        // Establish a connection to the Socket.IO server
+        const connection = io('https://streamtalk.onrender.com/');
 
-  socket?.on('connect_error', async (err) => {
-    console.log("Error establishing socket", err)
-    await fetch('/api/socket')
-  })
+        console.log("Socket connection", connection);
+        setSocket(connection);
 
-  return (
-    <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
-  );
+        // Handle connection error
+        connection.on('connect_error', async (err) => {
+            console.log("Error establishing socket", err);
+            await fetch('/api/socket'); 
+        });
+
+        // Cleanup on unmount
+        return () => {
+            connection.disconnect();
+        };
+    }, []);
+
+    return (
+        <SocketContext.Provider value={socket}>
+            {children}
+        </SocketContext.Provider>
+    );
 };
