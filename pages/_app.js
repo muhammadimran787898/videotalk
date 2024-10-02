438e0aa420ee0ecd04d3f8cb4bf217fb8d863c8b
Muhammad imran <muhammadimran787898@gmail.com>
2024-10-03 02:23:24 +0530

    Add-Title&Favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import "@/styles/globals.css";
-
 import { SocketProvider } from "@/context/socket";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
-    <SocketProvider>
-      <Component {...pageProps} />
-    </SocketProvider>
+    <>
+      <Head>
+        <title>StreamTalk</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <SocketProvider>
+        <Component {...pageProps} />
+      </SocketProvider>
+    </>
   );
 }
